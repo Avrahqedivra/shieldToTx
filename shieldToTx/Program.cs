a239427bfc0ed8926be2fe5f60557a03cafaf912
Jean-Michel Cohen <jmc_96@hotmail.com>
2021-06-07 11:31:31 +0200

    added french "usage" added capitalize name field option

--- shieldToTx/Program.cs
+++ shieldToTx/Program.cs
@@ -18,7 +18,10 @@
     along with Copyright Header.  If not, see <http://www.gnu.org/licenses/>.    
 */
 
+#define FRENCH
+
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 
@@ -26,15 +29,27 @@ namespace shieldToTx
 {
     class Program
     {
-        enum TxType: int { HD1 = 0, D868, D878, GD77, MD380 };
+        public enum TxType : int { HD1 = 0, D868, D878, GD77, MD380 };
+
+        public List<string> options = new List<string>();
 
         char separator = ',';
 
-        void parseContent(String content, TxType type)
+        bool capitalize()
+        {
+            for(int i=0; i < options.Count; i++)
+            {
+                if (options[i] == "m")
+                    return true;
+            }
+
+            return false;
+        }
+        public void parseContent(String content, TxType type)
         {
             String[] lines = content.Split("\r\n");
 
-            switch(type) { 
+            switch (type) {
                 case TxType.HD1:
                     Console.WriteLine("Radio ID,Callsign,Name,City,State,Country,Remarks");
                     break;
@@ -59,7 +74,9 @@ namespace shieldToTx
                     break;
             }
 
-            for (int i=0; i < lines.Length; i++)
+            bool capitalizeNames = capitalize();
+
+            for (int i = 0; i < lines.Length; i++)
             {
                 String[] user = lines[i].Split(separator);
 
@@ -77,6 +94,9 @@ namespace shieldToTx
                 }
 
                 name = name.Trim();
+
+                if (capitalizeNames)
+                    name = name.ToUpper();
 
                 String city = "";
                 String state = "";
@@ -118,7 +138,7 @@ namespace shieldToTx
                         break;
                 }
 
-                switch(type)
+                switch (type)
                 {
                     case TxType.HD1: // ailucne HD1
                         Console.WriteLine("Private Call," + shieldId + "," + name + "," + city + "," + state + "," + country + "," + dmrId);
@@ -142,14 +162,43 @@ namespace shieldToTx
 
                     default:
                         break;
-                }                    
+                }
             }
         }
 
-        void usage()
+        public void usage()
         {
+#if FRENCH
             Console.WriteLine("");
-            Console.WriteLine(" shieldToTx v1.3 - Convert TheShield (tm) OnLine Database to several CPS formats");
+            Console.WriteLine(" shieldToTx v1.4 - Converti la base de données en ligne TheShield (tm) dans plusieurs formats de CPS");
+            Console.WriteLine(" Copyright (c) 2021 Jean-Michel Cohen");
+            Console.WriteLine("");
+            Console.WriteLine(" Usage : ");
+            Console.WriteLine("");
+            Console.WriteLine("  - pour obtenir cette aide");
+            Console.WriteLine("");
+            Console.WriteLine("\tshieldToTx");
+            Console.WriteLine("");
+            Console.WriteLine("  - pour créer un fichier");
+            Console.WriteLine("");
+            Console.WriteLine("\tshieldToTx [0..4] > nomdefichier.csv");
+            Console.WriteLine("");
+            Console.WriteLine("  - pour concaténer à un fichier existant");
+            Console.WriteLine("");
+            Console.WriteLine("\tshieldToTx [0.." + sizeof(TxType) + "] >> nomdefichier.csv\t(note: ne pas oublier, s'il y a lieu, de supprimer la ligne d'entête de l'ajout)");
+            Console.WriteLine("");
+            Console.WriteLine("  - pour mettre le nom en majuscules ajouter l'option -m");
+            Console.WriteLine("");
+            Console.WriteLine("\tshieldToTx -m [0.." + sizeof(TxType) + "] > nomdefichier.csv");
+            Console.WriteLine("");
+            Console.WriteLine(" avec\t0 pour Ailunce HD1");
+            Console.WriteLine("\t1 pour AnyTone D868");
+            Console.WriteLine("\t2 pour AnyTone D878");
+            Console.WriteLine("\t3 pour OpenGD77");
+            Console.WriteLine("\t4 pour MD380, MD390, MD2017, RT82");
+#else
+            Console.WriteLine("");
+            Console.WriteLine(" shieldToTx v1.4 - Convert TheShield (tm) OnLine Database to several CPS formats");
             Console.WriteLine(" Copyright (c) 2021 Jean-Michel Cohen");
             Console.WriteLine("");
             Console.WriteLine(" Usage : ");
@@ -166,43 +215,92 @@ namespace shieldToTx
             Console.WriteLine("");
             Console.WriteLine("\tshieldToTx [0.."+ sizeof(TxType) + "] >> filename.csv\t(note: don't forget to remove the appended header line)");
             Console.WriteLine("");
+            Console.WriteLine("  - to capitalize the name add option -m");
+            Console.WriteLine("");
+            Console.WriteLine("\tshieldToTx -m [0.." + sizeof(TxType) + "] > filename.csv");
+            Console.WriteLine("");
             Console.WriteLine(" with\t0 for Ailunce HD1");
             Console.WriteLine("\t1 for AnyTone D868");
             Console.WriteLine("\t2 for AnyTone D878");
             Console.WriteLine("\t3 for OpenGD77");
             Console.WriteLine("\t4 for MD380, MD390, MD2017, RT82");
+#endif
         }
 
         static void Main(string[] args)
         {
             int num = 0;
-
             Program program = new Program();
 
             // check if enough parameters (i.e TX type)
-            if ((args.Length < 1) || !int.TryParse(args[0], out num) || (num > sizeof(TxType)))
+            if (args.Length < 1)
             {
                 program.usage();
                 System.Environment.Exit(1);
             }
 
+            for (int i = 0; i < args.Length; i++)
+            {
+                string option = args[i];
+
+                // check for options
+                if (option.StartsWith("-"))
+                {
+                    option = option.Substring(1);
+                    if (option.Length == 1)
+                    {
+                        switch (option)
+                        {
+                            case "m":   // Capitalize string
+                            case "c":   // Capitalize first char
+                                program.options.Add(option);
+                                break;
+
+                            default:
+                                program.usage();
+                                System.Environment.Exit(1);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        program.usage();
+                        System.Environment.Exit(1);
+                    }
+
+                }
+                else
+                {
+                    if (!int.TryParse(option, out num) || (num > sizeof(Program.TxType)))
+                    {
+                        program.usage();
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+
             String url = @"http://theshield.site/Liste_Shield.txt";
 
             WebClient client = new WebClient();
-            
+
             Stream stream = client.OpenRead(url);
             if (stream == null)
             {
                 Console.WriteLine("");
+#if FRENCH
                 Console.WriteLine(" Cannot load contact file from TheShield");
                 Console.WriteLine(" Check your internet connection!");
+#else
+            Console.WriteLine(" Impossible de télécharger la base de contacts du Shield");
+            Console.WriteLine(" Vérifier votre connexion internet !");
+#endif
                 Console.WriteLine("");
                 System.Environment.Exit(1);
             }
 
             StreamReader reader = new StreamReader(stream);
             String content = reader.ReadToEnd();
-            program.parseContent(content, (TxType)num);
+            program.parseContent(content, (Program.TxType)num);
 
             System.Environment.Exit(0);
         }
